c9556f00141313fb466f2be2f8eb5bd07ad6e26b
Raphael Höser <raphael@hoeser.info>
2021-07-07 18:01:06 +0200

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "serverless", // The serverless function name from your permalink object
+    functionsDir: "./netlify/functions/",
+  });
+};
